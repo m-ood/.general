8d5e04549a3f3468edc2cdb95d3f96e28b2a85b9
m-ood <86877763+m-ood@users.noreply.github.com>
2024-07-16 12:36:40 -0400

    Create flag-override.ps1

---
+++ files/scripts/ps1/flag-override.ps1
@@ -0,0 +1,18 @@
+&{$host.ui.rawui.BackgroundColor="Black";clear;};
+
+cd $PSScriptRoot;
+
+function k($j){$h=@{};foreach($a in ($j|ConvertFrom-Json).PSObject.Properties){$h[$a.name]=$a.value};return $h};
+
+function format-json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {$indent = 0;($json -Split "`n" | % {if ($_ -match '[\}\]]\s*,?\s*$') {$indent--};$line = ('  ' * $indent) + $($_.TrimStart() -replace '":  (["{[])', '": $1' -replace ':  ', ': ');if ($_ -match '[\{\[]\s*$') {$indent++};$line}) -Join "`n"};
+
+
+
+
+
+if ((test-path -path 'config.json') -eq $true){$s=(k(gc -Raw -Path .\config.json))} else {$s=@{}};if (($s.FflagsOverrideLocation -eq $null) -or ((test-path -path $s.FflagsOverrideLocation) -eq $false)){$s.FflagsOverrideLocation=".\fflags.jsonc";($s|convertto-json|format-json)>.\config.json;};
+
+
+$n=k((gc -Raw -Path fflags.jsonc)-replace'(?m)(?<=^([^"]|"[^"]*")*)//.*'-replace'(?ms)/\*.*?\*/');
+
+($n|convertto-json|format-json)>.\Modifications\ClientSettings\ClientAppSettings.json;
