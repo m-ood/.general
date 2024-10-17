1a5c6dd7c32633203785d4e6af8f64f1bc6ee574
m-ood <86877763+m-ood@users.noreply.github.com>
2024-10-17 19:58:15 -0400

    Update flag-override.ps1

--- files/scripts/ps1/flag-override.ps1
+++ files/scripts/ps1/flag-override.ps1
@@ -1,10 +1,7 @@
 &{$host.ui.rawui.BackgroundColor="Black";clear;};cd $PSScriptRoot;
 
-
 function k($j){$h=@{};foreach($a in ($j|ConvertFrom-Json).PSObject.Properties){$h[$a.name]=$a.value};return $h};
-
 function format-json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {$indent = 0;($json -Split "`n" | % {if ($_ -match '[\}\]]\s*,?\s*$') {$indent--};$line = ('  ' * $indent) + $($_.TrimStart() -replace '":  (["{[])', '": $1' -replace ':  ', ': ');if ($_ -match '[\{\[]\s*$') {$indent++};$line}) -Join "`n"};
-
 
 
 if ((test-path -path '.\config.json') -eq $true){$s=(k(gc -Raw -Path .\config.json))} else {$s=@{}};
@@ -13,8 +10,9 @@ if (($s.FflagsOverrideLocation -eq $null) -or ((test-path -path $s.FflagsOverrid
 
 if ((test-path -path $s.FflagsOverrideLocation) -eq $true) {
     $n=k((gc -Raw -Path $s.FflagsOverrideLocation)-replace'(?m)(?<=^([^"]|"[^"]*")*)//.*'-replace'(?ms)/\*.*?\*/');
-    [IO.File]::WriteAllLines('.\Modifications\ClientSettings\ClientAppSettings.json',(($n|convertto-json|format-json)));
+    [IO.File]::WriteAllLines($PSScriptRoot+"\Modifications\ClientSettings\ClientAppSettings.json",($n|convertto-json|format-json));
+    #($n|convertto-json|format-json) | out-file -encoding "ASCII" -NoNewline -Force -FilePath .\Modifications\ClientSettings\ClientAppSettings.json #? no trailing newline
 };
-& '.\BloxStrap.exe' -player "$($args[0])"
 
+& '.\BloxStrap.exe' -player "$($args[0])"
 
