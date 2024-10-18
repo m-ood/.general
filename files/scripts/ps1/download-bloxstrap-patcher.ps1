41c997e4ba40cc251b706d7032b287e7b84d49ac
m-ood <86877763+m-ood@users.noreply.github.com>
2024-10-17 23:03:39 -0400

    Add files via upload

--- files/scripts/ps1/download-bloxstrap-patcher.ps1
+++ files/scripts/ps1/download-bloxstrap-patcher.ps1
@@ -4,8 +4,8 @@ function format-json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {$
 ([System.Net.WebClient]::new()).downloadstring('https://raw.githubusercontent.com/m-ood/.general/main/files/scripts/ps1/strapon.ps1')>strapon.ps1;
 
 $s=@{};foreach($a in ((gc -Raw -Path .\settings.json)|ConvertFrom-Json).PSObject.Properties){$s[$a.name]=$a.value};
-for ($i=0; $i -lt $s.CustomIntegrations.Length; $i++) {if ($s.CustomIntegrations[$i].name -eq 'flag-override') {$int=1}};
-if ($int -eq $null){$s.CustomIntegrations+=(@{Name='flag-override';Location='cmd.exe';LaunchArgs='/c start /min "" powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -command "& sp -path ''Registry::HKCR\roblox-player\shell\open\command'' -name ''(Default)'' -value (''cmd.exe /c start /min \""\"" powershell.exe  -WindowStyle Hidden -ExecutionPolicy Bypass -file \""'' + $env:localappdata + ''\bloxstrap\strapon.ps1\"" \""%1\""'') " ';AutoClose=$false});};
+for ($i=0; $i -lt $s.CustomIntegrations.Length; $i++) {if ($s.CustomIntegrations[$i].name -eq 'strapon') {$int=1}};
+if ($int -eq $null){$s.CustomIntegrations+=(@{Name='strapon';Location='cmd.exe';LaunchArgs='/c start /min "" powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -command "& sp -path ''Registry::HKCR\roblox-player\shell\open\command'' -name ''(Default)'' -value (''cmd.exe /c start /min \""\"" powershell.exe  -WindowStyle Hidden -ExecutionPolicy Bypass -file \""'' + $env:localappdata + ''\bloxstrap\strapon.ps1\"" \""%1\""'') " ';AutoClose=$false});};
 $s.ConfirmLaunches=$false
 ($s|convertto-json)>.\settings.json
 if ((test-path -path '.\config.json') -eq $false) {(@{FflagsOverrideLocation='.\fflags.jsonc'}|convertto-json|format-json)>.\config.json;};
@@ -23,3 +23,4 @@ if ((test-path -path '.\mods') -eq $false) {
 #cmd.exe /c start /min "" powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -file "C:\Users\memaz\AppData\Local\bloxstrap\strapon.ps1" "%1"
 
 
+## iex([System.Net.WebClient]::new()).downloadstring('https://raw.githubusercontent.com/m-ood/.general/refs/heads/main/files/scripts/ps1/download-bloxstrap-patcher.ps1');
