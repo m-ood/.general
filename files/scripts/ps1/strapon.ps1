798f12c0597c884bffcada9dccacb974413ebf9d
m-ood <86877763+m-ood@users.noreply.github.com>
2024-10-18 17:18:47 -0400

    Add files via upload

--- files/scripts/ps1/strapon.ps1
+++ files/scripts/ps1/strapon.ps1
@@ -1,8 +1,9 @@
 $host.ui.rawui.BackgroundColor="Black";cd $PSScriptRoot;
 
-(ls '.\mods').Name|%{&".\mods\$($_)"}
+(ls '.\mods').Name|%{&".\mods\$($_)" "$($args[0])"}
 & '.\BloxStrap.exe' -player "$($args[0])"
 $($args[0])>.\Logs\uri.txt
 
 echo finished
+
 return
